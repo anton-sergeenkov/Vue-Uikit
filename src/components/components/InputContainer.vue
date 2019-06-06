b971778db1eb502a66896c487c67b72870f99bf8
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2019-06-06 16:06:58 +0300

    z

--- src/components/components/InputContainer.vue
+++ src/components/components/InputContainer.vue
@@ -6,8 +6,7 @@
         <!---------------------------------------->
 
         <h2>theme="normal | primary"</h2>
-        {{inputText}}
-        <ui-input theme="normal" v-model="inputText" placeholder="Typed..." autofocus></ui-input>
+        <ui-input type="text" theme="normal" v-model="inputText" placeholder="Typed..."></ui-input>
         <ui-input theme="primary" placeholder="Typed..."></ui-input>
     </div>
 
@@ -19,18 +18,13 @@ export default {
         return {
             inputText: ''
         }
-    },
-    methods: {
-        handlerClick() {
-            alert('ok');
-        }
     }
 }
 </script>
 
 <style>
 
-.ui-input {
-    margin: 10px;
+.input-wrapper {
+    margin: 30px 0px;
 }
 </style>
--- src/components/ui/Input.vue
+++ src/components/ui/Input.vue
@@ -1,13 +1,31 @@
 <template>
-    <input :class="classes" :value="value" @input="$emit('input', $event.target.value)" class="ui-input">
+    <div class="input-wrapper">
+        <input :class="classes" :value="value" @focus="onFocus" @blur="onBlur($event.target.value)" @input="$emit('input', $event.target.value)" class="ui-input">
+        <label :class="{ active:focused }">Name</label>
+    </div>
 </template>
 
 <script>
 export default {
+    data() {
+        return {
+            focused: false
+        }
+    },
     props: {
         theme: { type: String, default: 'normal' },
         block: { type: Boolean },
         value: { type: String }
+    },
+    methods: {
+        onFocus() {
+            this.focused = true;
+        },
+        onBlur(content) {
+            if (content === '') {
+                this.focused = false;
+            }
+        }
     },
     computed: {
         classes() {
@@ -19,37 +37,61 @@ export default {
 }
 </script>
 
-<style scoped>
+<style scoped scoped lang="scss">
+$color-light: #747474;
+$font-size: 15px;
+$color-accent: #1867c0;
+
 .ui-input {
-    --color-bg-wrapper:  rgba(0,0,0,0.3);
-    --font-input:        15px;
-    --color-light:       #eee;
-}
-.ui-input{
     width: 100%; 
-    padding: 15px;
+    padding: 10px;
     border: none;
     border-radius: 2px;
     font-family: inherit;
-    font-size: var(--font-input);
+    font-size: $font-size;
     outline: none;
+    z-index: 1;
+    position: relative;
+    background: none;
 }
+.input-wrapper {
+    width: 100%;
+    position: relative;
+}
+label {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 10px;
+    transition: 0.4s;
+    z-index: 0;
+    font-size: $font-size;
+}
+label.active {
+    top: -30px;
+    font-size: 13px;
+    color: $color-accent;
+}
+// .ui-input:focus {
+//     border-bottom: 1px solid #1867c0;
+// }
+
+
 
 /*----------------------------------------
 NORMAL 
 ----------------------------------------*/
 .ui-input--normal {
-    border: 1px solid #000;
+    border-bottom: 1px solid $color-light;
 }
 
 /*----------------------------------------
 PRIMARY 
 ----------------------------------------*/
 .ui-input--primary {
-    background: var(--color-light);
-    border-bottom: 2px solid transparent;
+    border-bottom: 1px solid yellowgreen;
 }
 .ui-input--primary:focus {
-    border-bottom: 2px solid var(--color-bg-wrapper);
+    border-bottom: 1px solid black;
 }
 </style>

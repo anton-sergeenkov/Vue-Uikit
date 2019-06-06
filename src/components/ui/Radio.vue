ccb370aa4218221cf747aaf2ac6bc1e9cf0cbca5
Anton <45148047+anton-sergeenkov@users.noreply.github.com>
2019-06-06 18:29:50 +0300

    Update Radio.vue

--- src/components/ui/Radio.vue
+++ src/components/ui/Radio.vue
@@ -30,6 +30,10 @@ export default {
 </script>
 
 <style scoped lang="scss">
+$c-primary: #005caf;
+$c-dark: #444444;
+$width: 25px;
+
 .wrapper-radio {
     display: flex;
 }
@@ -39,15 +43,18 @@ input[type="radio"] {
 input[type="radio"]:checked + label:after {
     content: '';
     display: block;
-    background: white;
-    width: 30px;
-    height: 30px;
+    background: $c-primary;
+    width: $width / 1.75;
+    height: $width / 1.75;
     border-radius: 50%;
 }
+input[type="radio"]:checked + label {
+    border: 2px solid $c-primary;
+}
 label {
-    width: 50px;
-    height: 50px;
-    background: yellowgreen;
+    width: $width;
+    height: $width;
+    border: 2px solid $c-dark;
     border-radius: 50%;
     display: flex;
     justify-content: center;
